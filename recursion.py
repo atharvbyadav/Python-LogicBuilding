f52fde541f0463c08ab8983a8b55d37d10a9b5bc
atharvbyadav <atharvbyadav@gmail.com>
2025-04-19 13:06:37 +0530

    Neww Stuff

--- recursion.py
+++ recursion.py
@@ -36,3 +36,31 @@ def count_down(n):
 
 count_down(10)
 
+li = [1, 2, 3, 4, 5]
+
+def sum_li(li):
+    if len(li) == 0:
+        return 0
+    else:
+        return li[0] + sum_li(li[1:])
+
+print(sum_li(li))
+
+# def sum_list(li):
+#     if len(li) == 0:
+#         return 0
+#     else:
+#         x = len(li)
+#         return li[0] + sum_list(x)
+
+# print(sum_list(li))
+
+def count_down(n):
+    if n > 10:
+        print("Blast off!")
+        return 0
+    else:
+        print(n)
+        return n + count_down(n+1)
+
+count_down(1)
